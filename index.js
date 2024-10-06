e8f1de4bbb8d19ea32bea45785a043a40a8d194f
mdasif6565 <mdasifdev123@gmail.com>
2024-10-06 17:43:02 +0600

    flovite link

--- index.js
+++ index.js
@@ -11,24 +11,6 @@ function closeMenu() {
 }
 
 
-var sideMenu1 = document.getElementById('side-menu-1');
-function openMenu1() {
-    sideMenu1.classList.remove('right-[-440px]');
-    sideMenu1.classList.add('right-0');
-}
-
-function closeMenu1() {
-    sideMenu1.classList.remove('right-0');
-    sideMenu1.classList.add('right-[-440px]');
-}
-
-
-
-var sideMenu2 = document.getElementById('side-menu-2');
-function openMenu2() {
-    sideMenu2.classList.remove('left-[-250px]');
-    sideMenu2.classList.add('left-0');
-}
 
 function closeMenu2() {
     sideMenu2.classList.remove('left-0');
