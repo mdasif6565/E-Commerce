20cedcd6c854965872b5120b79dc9dac9fde4c56
mdasif6565 <mdasifdev123@gmail.com>
2024-10-10 19:06:16 +0600

    menu bar problem fixed

--- index.js
+++ index.js
@@ -10,14 +10,27 @@ function closeMenu() {
     sideMenu.classList.add('right-[-250px]');
 }
 
+var sideMenu1 = document.getElementById('side-menu-1');
+function openMenu1() {
+    sideMenu1.classList.remove('right-[-250px]');
+    sideMenu1.classList.add('right-0');
+}
 
+function closeMenu1() {
+    sideMenu1.classList.remove('right-0');
+    sideMenu1.classList.add('right-[-250px]');
+}
+
+var sideMenu2 = document.getElementById('side-menu-2');
+function openMenu2() {
+    sideMenu2.classList.remove('left-[-250px]');
+    sideMenu2.classList.add('left-0');
+}
 
 function closeMenu2() {
     sideMenu2.classList.remove('left-0');
     sideMenu2.classList.add('left-[-250px]');
 }
-
-
 const slider = document.getElementById('slider');
 const images = slider.children;
 const totalImages = images.length;
--- index2.js
+++ index2.js
@@ -1,19 +1,21 @@
-  
-  const tabs = document.querySelectorAll('.tab-link');
-  const contents = document.querySelectorAll('.tab-content');
+var sideMenu1 = document.getElementById('side-menu-1');
+function openMenu1() {
+    sideMenu1.classList.remove('right-[-250px]');
+    sideMenu1.classList.add('right-0');
+}
 
-  tabs.forEach(tab => {
-      tab.addEventListener('click', function(event) {
-          event.preventDefault();
+function closeMenu1() {
+    sideMenu1.classList.remove('right-0');
+    sideMenu1.classList.add('right-[-250px]');
+}
 
-          // Remove active class from all tabs and contents
-          tabs.forEach(t => t.classList.remove('text-blue-600', 'border-blue-600'));
-          contents.forEach(c => c.classList.remove('active'));
+var sideMenu2 = document.getElementById('side-menu-2');
+function openMenu2() {
+    sideMenu2.classList.remove('left-[-250px]');
+    sideMenu2.classList.add('left-0');
+}
 
-          // Add active class to the clicked tab and corresponding content
-          this.classList.add('text-blue-600', 'border-blue-600');
-          const tabId = this.getAttribute('data-tab');
-          document.getElementById(tabId).classList.add('active');
-      });
-  });
-
+function closeMenu2() {
+    sideMenu2.classList.remove('left-0');
+    sideMenu2.classList.add('left-[-250px]');
+}
